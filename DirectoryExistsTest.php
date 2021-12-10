aab8da5bca4fa65a2f6520720afd802ae474cc64
georgeartdavidson00 <53605763+georgeartdavidson00@users.noreply.github.com>
2021-12-10 05:07:30 +0000

    Sessionupdate1

---
+++ DirectoryExistsTest.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+final class DirectoryExistsTest extends TestCase
+{
+    public function testFailure(): void
+    {
+        $this->assertDirectoryExists('php');
+        $this->assertDirectoryExists('webforms');
+    }
+}
